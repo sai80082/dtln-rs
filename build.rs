5156e9fa0cfbe54453bd5b53def7f306a6f79790
Sai charan Guntupalli <63032681+sai80082@users.noreply.github.com>
2025-11-30 17:02:15 +0530

    Implement CMake build process for specific OS
    Add CMake build support for non-Windows and non-macOS systems.

--- build.rs
+++ build.rs
@@ -3,6 +3,37 @@ fn main() {
     // NOP on windows
 }
 
+#[cfg(all(not(target_os = "macos"), not(target_os = "windows")))]
+fn main() {
+    use std::{env, process::Command};
+
+    Command::new("cmake")
+        .current_dir("tflite")
+        .arg(".")
+        .arg("-DCMAKE_BUILD_TYPE=Release")
+        .status()
+        .expect("Failed to run cmake");
+
+    let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}/tflite/lib/", root_dir);
+
+    std::fs::read_dir(std::format!("{}/tflite/lib", root_dir))
+        .unwrap()
+        .for_each(|entry| {
+            let path = entry.unwrap().path();
+            let extension = path.extension();
+            match extension {
+                Some(ext) if ext == "a" => {
+                    let lib_name = path.file_stem().unwrap().to_str().unwrap();
+                    if let Some(lib_name) = lib_name.strip_prefix("lib") {
+                        println!("cargo:rustc-link-lib=dylib={}", lib_name);
+                    }
+                }
+                _ => {}
+            };
+        });
+}
+
 #[cfg(target_os = "macos")]
 fn main() {
     use std::{env, process::Command};
